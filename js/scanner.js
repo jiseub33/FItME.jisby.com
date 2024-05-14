02e4784590549e9b33ef65fbf2266633fcaa7439
jiseub33 <jiseub33@gmail.com>
2024-05-15 05:26:05 +0900

    update

--- js/scanner.js
+++ js/scanner.js
@@ -1,40 +1,17 @@
-// barcode-scanner.js
-
-// Function to initialize barcode scanner
-function initializeScanner() {
-    const video = document.getElementById('barcode-video');
-
-    // Check if getUserMedia is available
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-            .then(function (stream) {
-                // Set the video source to the camera stream
-                video.srcObject = stream;
-
-                // Initialize barcode scanner library (e.g., QuaggaJS)
-                // Set up event listeners to handle barcode detection
-
-                // Example event listener for successful barcode detection
-                video.addEventListener('barcode-detected', function(event) {
-                    const barcodeNumber = event.detail.barcodeNumber;
-                    navigateToURL(barcodeNumber);
-                });
-            })
-            .catch(function (error) {
-                console.error('Error accessing camera:', error);
-            });
-    } else {
-        console.error('getUserMedia is not supported');
-    }
-}
-
-// Function to navigate to URL with barcode number
-function navigateToURL(barcodeNumber) {
-    const url = 'https://fitme.jisby.com/' + barcodeNumber; // Replace 'example.com' with your actual domain
-    window.location.href = url;
-}
-
-// Call the initializeScanner function when the page loads
-window.onload = function () {
-    initializeScanner();
+const html5QrCode = new Html5Qrcode("reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    /* handle success */
 };
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+// If you want to prefer front camera
+html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+
+// If you want to prefer back camera
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+// Select front camera or fail with `OverconstrainedError`.
+html5QrCode.start({ facingMode: { exact: "user"} }, config, qrCodeSuccessCallback);
+
+// Select back camera or fail with `OverconstrainedError`.
+html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
